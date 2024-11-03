67164f0ee41e3b96827f5a3a3a5e76879c20c9bf
Nakorn Sinpadung <nakorndev@gmail.com>
2024-11-03 18:13:43 +0700

    using commander

--- read-dir.js
+++ read-dir.js
@@ -1,13 +1,24 @@
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-d, --dir [dir]', 'directory to read')
+  .parse(process.argv);
+
 var fs = require('fs');
 
-var args = process.argv;
-var dir = args[2];
+var dir = program.dir;
 
-fs.readdir(dir, function (err, files) {
-  if (err) {
-    throw err;
-  }
-  files.forEach(function (file) {
-    console.log(file);
+if (dir) {
+  fs.readdir(dir, function (err, files) {
+    if (err) {
+      throw err;
+    }
+    files.forEach(function (file) {
+      console.log(file);
+    });
   });
-});
+} else {
+  console.log('Error: Please specify the directory using -d or --dir');
+}
+
