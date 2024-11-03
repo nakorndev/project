edc5351cf9dc9a7ffdbf8ee80b325ddf48802300
Nakorn Sinpadung <nakorndev@gmail.com>
2024-11-03 18:25:10 +0700

    add routing /hello/:name

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  var url = require('url');
+  var pathname = url.parse(req.url).pathname;
+  if (pathname == '/hello/:name') {
+    var name = pathname.split('/').pop();
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello ' + name + '\n');
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:3000/');
+
